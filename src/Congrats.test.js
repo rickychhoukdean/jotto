8d2d433afec8c5eea081522b671bf26e4169b9d0
rickychhoukdean <chhoukdean184@msn.com>
2019-11-13 23:30:58 -0500

    TDD create three tests

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,21 +2,27 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import Congrats from "./Congrats";
-import {findByTestAttr} from "../test/testutils"
+import { findByTestAttr } from "../test/testutils";
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const setup = (props={})  =>{
-    return shallow(<Congrats{...props}/>)
-}
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
 
-test('renders without error',()=>{
+test("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1);
+});
 
-})
+test("renders no text when success message is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe(" ");
+});
 
-test('renders no text when success message is false',()=>{
-
-})
-
-test('renders nonempty message congrats message when success prop is true',()=>{
-
-})
+test("renders nonempty message congrats message when success prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});

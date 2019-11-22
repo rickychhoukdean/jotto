2d61b82719407fe29b9f3884ebded62080bdcba2
rickychhoukdean <chhoukdean184@msn.com>
2019-11-22 15:10:04 -0500

    Add test cases

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -14,3 +14,21 @@ const setup = (props = {}) => {
 test("does not throw warning with expected props", () => {
   checkProps(GuessedWords, defaultProps);
 });
+
+describe("if there are not words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWord: [] });
+  });
+
+  test("should render without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("should render instructions when a word is guessed", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().lenghth).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {});

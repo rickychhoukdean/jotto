11ad255742557a16c573fb4e67fefa1b04adf3f9
rickychhoukdean <chhoukdean184@msn.com>
2019-11-25 15:19:14 -0500

    Add Bootstrap styling

--- src/Congrats.js
+++ src/Congrats.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 function Congrats(props) {
   if (props.success) {
     return(
-    <div data-test="component-congrats">
+    <div data-test="component-congrats" className="alert alert-success">
       <span data-test="congrats-message">Congrats you guessed the word</span>
     </div>);
   } else {

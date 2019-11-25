15403f875fe7133e1fb2b7223f04fdaaf050c5af
rickychhoukdean <chhoukdean184@msn.com>
2019-11-25 17:35:19 -0500

    Set up Input component and storeFactory test util

--- test/testutils.js
+++ test/testutils.js
@@ -1,14 +1,22 @@
 import checkPropTypes from "check-prop-types";
+import { createStore } from "redux";
+
+import rootReducer from "../src/reducers";
+
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+};
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
 
 export const checkProps = (component, conformingProps) => {
-    const propError = checkPropTypes(
-      component.propTypes,
-      conformingProps,
-      'prop',
-      component.name);
-    expect(propError).toBeUndefined();
-  }
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
+};
